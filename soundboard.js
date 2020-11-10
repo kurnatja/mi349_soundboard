08cc94e50e7aed87128d698272fd8548dd95f9d0
kurnatja <kurnatja@msu.edu>
2020-11-10 15:57:57 -0500

    fixing validation error

--- soundboard.js
+++ soundboard.js
@@ -1,19 +1,19 @@
-var buttonElementOne = document.getElementById('button-one')
-var buttonElementTwo = document.getElementById('button-two')
-var buttonElementThree = document.getElementById('button-three')
+var buttonElementOne = document.getElementById('button-one');
+var buttonElementTwo = document.getElementById('button-two');
+var buttonElementThree = document.getElementById('button-three');
 
-var doorbell = new Audio('doorbell_x.wav')
-var gong = new Audio('gong.wav')
-var peeeooop = new Audio('peeeooop_x.wav')
+var doorbell = new Audio('doorbell_x.wav');
+var gong = new Audio('gong.wav');
+var peeeooop = new Audio('peeeooop_x.wav');
 
 buttonElementOne.addEventListener('click', function() {
-    doorbell.play()
-})
+    doorbell.play();
+});
 
 buttonElementTwo.addEventListener('click', function() {
-    gong.play()
-})
+    gong.play();
+});
 
 buttonElementThree.addEventListener('click', function() {
-    peeeooop.play()
-})
+    peeeooop.play();
+});
